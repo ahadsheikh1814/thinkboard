1ad68d7934315b980a613ff0b58513ed88f6b7fb
ahadsheikh1814 <ahadsheikh1814@outlook.com>
2025-08-01 17:05:56 +0600

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,30 @@ import notesRouter from './router/notesRouter.js';
 import { connectDB } from './config/db.js';
 import ratelimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 dotenv.config();
 
+const __dirname = path.resolve();
 
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-}))
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }))
+}
 app.use(express.json());
 app.use(ratelimiter)
 
 app.use('/api/notes', notesRouter);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend","dist","index.html"))
+  })
+}
+
 
 connectDB().then(()=>{
   app.listen(5001, () => {
